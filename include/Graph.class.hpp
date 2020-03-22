baf629fc9648b402e3a3af09fa6ed0012da76f0f
Anas Rchid <archid-@student.1337.ma>
2020-03-22 21:47:28 +0100

    document the source code

--- include/Graph.class.hpp
+++ include/Graph.class.hpp
@@ -6,7 +6,7 @@
 //   By: archid- <archid-@student.1337.ma>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2020/03/16 00:48:27 by archid-           #+#    #+#             //
-//   Updated: 2020/03/22 17:12:56 by archid-          ###   ########.fr       //
+//   Updated: 2020/03/22 22:56:28 by archid-          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -24,35 +24,59 @@
 
 using namespace std;
 
+// represents a graph data structure, and provides basic graph manipulation
+// functions such as adding vertices and edges, as well as some standard
+// algorithms such as BFS, Dijkstra and Trajans SCC.
 class Graph
 {
+    // this is an internal structure that to represent each vertex
+    // it holds the vertex identity as well as its adjacent vertices
 	struct Vertex
 	{
 		Vertex(string s) : self(s) {}
 
-		vector<pair<int, Vertex *>> adj;
+        // vertex's identity
 		string self;
+
+        // vector of pairs of weight and destination vertices
+		vector<pair<int, Vertex *>> adj;
 	};
+
+    // representing the graph as a map helps to the efficiency of retrieving
+    // and manipulating vertices
 	map<string, Vertex *> g;
+
+    // internal protocol to trace back a path starting from the destination
+    // vertex up until the source vertex
+    list<string> construct_path(map<string, string>& parent,
+                                const string &s, const string &t);
+
+    // a custom DFS to set the component's id, used by SCC to find the
+    // strongly connected components in the graph
     void sccDFS(Vertex *e, vector<list<string>>& scc, vector<string>& vec,
                 int& id, map<string, pair<int, int>>& links);
 
 public:
 
-	Graph();
-	~Graph();
-
-    static Graph *readGraph(const char *file, bool directed = false,
-                            bool weighted = false);
+	Graph(); ~Graph();
     void dumpGraph();
     pair<string, string> getVertexPair();
 
+    // fill a graph with vertices and edges from the given file
+    // the graph properties, namely directness and weight could be
+    // specified as well.
+    static Graph *readGraph(const char *file, bool directed = false,
+                            bool weighted = false);
+
+    // add the vertex s to the graph, if it's not already there
 	bool addVertex(const string& s);
+
+    // add an edge between the two given vertices, weight and direction
+    // could be specified as well
 	void addEdge(const string& from, const string& to,
 				 int w = 1, bool directed = false);
 
-	// Breadth First Search is an elemnetary Graph algorithm that's perfect
-	// for finding the shortest path in an "unweighted graph".
+	// Breadth First Search
 	list<string> BFS(const string& s, const string& t);
 
 	// Dijkstra Shortest Path
--- src/Graph.class.cpp
+++ src/Graph.class.cpp
@@ -6,7 +6,7 @@
 //   By: archid- <archid-@student.1337.ma>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2020/03/16 00:49:43 by archid-           #+#    #+#             //
-//   Updated: 2020/03/22 17:17:37 by archid-          ###   ########.fr       //
+//   Updated: 2020/03/23 08:31:33 by archid-          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -20,6 +20,7 @@ Graph::Graph() : g() {}
 
 Graph::~Graph() {
 	for (auto itr = g.begin(); itr != g.end(); itr++)
+        // vertices were allocated dynamically so they must be free'd
 		delete itr->second;
 }
 
@@ -29,8 +30,12 @@ Graph *Graph::readGraph(const char *file, bool directed, bool weighted) {
     string s;
 
     if (!in) exit(-1);
+
+    // basically each line of the file is u v w
+    // so we read line by line and add edges
     while (in && getline(in, s)) {
         string u, v, w;
+
         u = string(s.substr(0, s.find(" ")));
         s.erase(0, s.find(" ") + 1);
         v = string(s.substr(0, s.find(" ")));
@@ -70,14 +75,18 @@ void Graph::addEdge(const string& from, const string& to, int w,
 		g.find(to)->second->adj.push_back(make_pair(w, g.find(from)->second));
 }
 
-list<string> construct_path(map<string, string>& parent,
-							const string &s, const string &t) {
+list<string> Graph::construct_path(map<string, string>& parent,
+                                   const string &s, const string &t) {
 	list<string> path;
 	string walk = t;
 
+    // destination not have no parent, thus it hasn't been reached
 	if (parent.find(t) == parent.end())
 		return path;
+    // keep tracing back the parents until we arrive.
 	while (true) {
+        // pushing vertices before testing on arrival to include
+        // source and destination nodes
 		path.push_front(walk);
 		if (walk == s)
 			break;
@@ -98,6 +107,9 @@ list<string> Graph::BFS(const string &s, const string &t) {
 		if (q.front() == t) break;
 		auto neighbors = g.find(q.front())->second->adj;
 		for (auto itr = neighbors.begin(); itr != neighbors.end(); itr++) {
+            // this plays the role of marking vertices as seen, since we
+            // only set the parent once, thus we can ignore vertices that
+            // already have a parent
 			if (parent.find(itr->second->self) != parent.end())
 				continue;
 			q.push(itr->second->self);
@@ -108,52 +120,77 @@ list<string> Graph::BFS(const string &s, const string &t) {
 	return construct_path(parent, s, t);
 }
 
+
 list<string> Graph::Dijkstra(const string& s, const string &t) {
 	map<string, int> dist;
 	map<string, string> parent;
-	priority_queue<pair<string, int>> pq;
+	priority_queue<pair<int, string>,
+                   vector<pair<int, string>>,
+                   greater<pair<int, string>>> pq;
 
 	if (g.find(s) == g.end())
 		return {};
-	dist[s] = 0.0;
-	pq.push({s, 0.0});
+	dist[s] = 0;
+	pq.push({0, s});
 	while (!pq.empty()) {
 		auto e = pq.top(); pq.pop();
-		if (dist.find(e.first) != dist.end() && dist[e.first] < e.second)
+        // we can ignore vertices that already have a minimal distance,
+        // this is a lazy way of getting rid of outdated distances
+		if (dist.find(e.second) != dist.end() && dist[e.second] < e.first)
 			continue;
-		auto neighbors = g.find(e.first)->second->adj;
+		auto neighbors = g.find(e.second)->second->adj;
 		for (auto iter = neighbors.begin(); iter != neighbors.end(); iter++) {
-			if (dist.find(iter->second->self) != dist.end())
-				continue;
-			int cost = dist[e.first] + iter->first;
-			if (dist.find(iter->second->self) == dist.end()
-					|| cost < dist[iter->second->self]) {
-				parent[iter->second->self] = e.first;
+            // initialize all undiscovered vertices to infinity
+            if (dist.find(iter->second->self) == dist.end())
+                 dist[iter->second->self] = numeric_limits<int>::max();
+            // computing the new distance and perform the edges relaxation
+            // simply by choosing the min distance
+			int cost = dist[e.second] + iter->first;
+			if (cost < dist[iter->second->self]) {
+				parent[iter->second->self] = e.second;
 				dist[iter->second->self] = cost;
-				pq.push({iter->second->self, cost});
+                // update the best distance by enqueuing the new
+                // distance. Although this might cause duplication
+				pq.push({cost, iter->second->self});
 			}
 		}
+        // since we traverse always the next most promising vertex by
+        // dequeuing from the pq, once we reach the destination, it's
+        // cost will never change
+        if (e.second == t)
+            break;
 	}
 	return construct_path(parent, s, t);
 }
 
-void Graph::sccDFS(Vertex *e, vector<list<string>>& scc, vector<string>& vec,
+void Graph::sccDFS(Vertex *e, vector<list<string>>& scc, vector<string>& v,
                    int& id, map<string, pair<int, int>>& links) {
-	vec.push_back(e->self);
+    // we push all vertices to the vector, so that we can tell which
+    // vertices we're currently following
+	v.push_back(e->self);
 	links[e->self] = {id, id};
 	id++;
+
+    // Traverse all the adjacent vertices in a DFS fashion
 	for (auto itr = e->adj.begin(); itr != e->adj.end(); itr++) {
 		if (links.find(itr->second->self) == links.end())
-			sccDFS(itr->second, scc, vec, id, links);
-		if (find(vec.begin(), vec.end(), itr->second->self) != vec.end())
+			sccDFS(itr->second, scc, v, id, links);
+        // on the callback, we set the lowest_id to be the min between
+        // the neighbors lowest, and self's lowest.
+        // this process happens recursively so that all the vertices
+        // that belong to the same connected component have same lowest id
+		if (find(v.begin(), v.end(), itr->second->self) != v.end())
 			links[e->self].second = min(links[e->self].second,
-									 links[itr->second->self].second);
+                                        links[itr->second->self].second);
 	}
+
+    // once we return finish the recursive DFS, and return to the
+    // initial callback, we pop from the visiting vector all the
+    // vertices that belong to the connected component
 	if (links[e->self].first == links[e->self].second) {
 		list<string> lst;
-		for (auto node = vec.back(); ; node = vec.back()) {
-			vec.pop_back();
-			links[node].second = links[node].first;
+		for (auto node = v.back(); ; node = v.back()) {
+			v.pop_back();
 			lst.push_back(node);
 			if (node == e->self)
 				break;
@@ -163,13 +200,15 @@ void Graph::sccDFS(Vertex *e, vector<list<string>>& scc, vector<string>& vec,
 }
 
 vector<list<string>> Graph::SCC() {
-	map<string, pair<int, int>> links;
-	vector<string> vec;
+	map<string, pair<int, int>> links; // a pair of <id, lowest_id>
+	vector<string> visiting;
 	vector<list<string>> scc;
 	int id = 0;
 
 	for (auto itr = g.begin(); itr != g.end(); itr++)
+        // each time we start at a different node, but only if it's
+        // not a part of an already found connected component
 		if (links.find(itr->first) == links.end())
-            sccDFS(itr->second, scc, vec, id, links);
+            sccDFS(itr->second, scc, visiting, id, links);
 	return scc;
 }
